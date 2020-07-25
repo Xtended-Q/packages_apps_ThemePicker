32996d2b1ea3af05de7055f8ea2e68c4f7615beb
Varun Date <date.varun123@gmail.com>
2020-07-25 23:51:09 +1000

    Themepicker: Re-direct accent to Xtensions ThemeRoom
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/customization/model/theme/custom/ThemeComponentOption.java
+++ src/com/android/customization/model/theme/custom/ThemeComponentOption.java
@@ -30,6 +30,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.Resources.Theme;
 import android.content.res.TypedArray;
+import android.content.Intent;
 import android.graphics.Path;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
@@ -42,6 +43,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -317,6 +319,9 @@ public abstract class ThemeComponentOption implements CustomizationOption<ThemeC
 
         private String mLabel;
 
+
+        private static final String SETTINGS_FRAG = "com.android.settings";
+        private static final String SETTINGS_ACTION = "com.android.settings.Settings$XThemeRoomActivity";
         ColorOption(String packageName, String label, @ColorInt int lightColor,
                 @ColorInt int darkColor) {
             addOverlayPackage(OVERLAY_CATEGORY_COLOR, packageName);
@@ -343,7 +348,16 @@ public abstract class ThemeComponentOption implements CustomizationOption<ThemeC
                     new int[] {-android.R.attr.state_activated}, unselectedOption);
 
             ((ImageView) view.findViewById(R.id.option_tile)).setImageDrawable(stateListDrawable);
+
             view.setContentDescription(mLabel);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent settings = new Intent(Intent.ACTION_MAIN);
+                    settings.setClassName(SETTINGS_FRAG, SETTINGS_ACTION);
+                    view.getContext().startActivity(settings);
+                }
+             });
         }
 
         @ColorInt
